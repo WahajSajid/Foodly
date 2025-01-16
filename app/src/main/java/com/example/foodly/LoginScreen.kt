cd50ae784f84c2feedef1896aa856aae4d73a35f
Wahaj Sajid <mwahajsajidali@gmail.com>
2025-01-17 02:42:31 +0500

    Working on authentication system

--- app/src/main/java/com/example/foodly/LoginScreen.kt
+++ app/src/main/java/com/example/foodly/LoginScreen.kt
@@ -36,7 +36,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.foodly.ui.theme.appThemeColor1
